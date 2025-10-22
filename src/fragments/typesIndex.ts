8adcaa85fe55777f14dfd4eee0d30beda5631b29
0xPratik <sariapratik@gmail.com>
2025-10-22 20:02:12 +0545

    added some logs to understand the flow better

---
+++ src/fragments/typesIndex.ts
@@ -0,0 +1,15 @@
+import { ProgramNode, camelCase } from '@codama/nodes';
+
+import { Fragment, fragment } from '../utils';
+
+export function getTypesIndexFragment(node: ProgramNode): Fragment {
+    if (node.definedTypes.length === 0) {
+        return fragment``;
+    }
+
+    const exports = node.definedTypes.map(type => {
+        return fragment`export * from './${camelCase(type.name)}';`;
+    });
+
+    return fragment`${exports.map(f => f.content).join('\n')}`;
+}
